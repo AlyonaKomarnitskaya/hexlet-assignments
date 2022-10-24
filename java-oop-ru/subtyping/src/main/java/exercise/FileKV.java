4622a0c9896f74e5e0715c853df4d013a99d28cc
@hexlet/cli <support@hexlet.io>
2022-10-25 01:48:30 +0500

    submit java-oop-ru/subtyping

---
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -0,0 +1,52 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+public class FileKV implements KeyValueStorage {
+
+    private String path;
+    private Map<String, String> dictionary = new HashMap<>();
+
+    public FileKV(String path, Map<String, String> dictionary) {
+        this.path = path;
+        this.dictionary.putAll(dictionary);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        String readPath = Utils.readFile(path);
+        Map<String, String> map = Utils.unserialize(readPath);
+        map.put(key, value);
+        Utils.writeFile(this.path, Utils.serialize(map));
+
+    }
+
+    @Override
+    public void unset(String key) {
+        String readPath = Utils.readFile(path);
+        Map<String, String> map = Utils.unserialize(readPath);
+        map.remove(key);
+        Utils.writeFile(this.path, Utils.serialize(map));
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        String readPath = Utils.readFile(path);
+        Map<String, String> map = Utils.unserialize(readPath);
+        if (map.containsKey(key)) {
+            return map.get(key);
+        } else {
+            return defaultValue;
+        }
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        String readPath = Utils.readFile(path);
+        Map<String, String> map = Utils.unserialize(readPath);
+        return map;
+    }
+}
+// END
---
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import java.util.Map;
+import java.util.HashMap;
+
+// BEGIN
+public class InMemoryKV implements KeyValueStorage {
+
+    private Map<String, String> storage = new HashMap<>();
+
+    public InMemoryKV(Map<String, String> storage) {
+        this.storage.putAll(storage);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        storage.put(key, value);
+    }
+
+    @Override
+    public void unset(String key) {
+        storage.remove(key);
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        if (storage.containsKey(key)) {
+            return storage.get(key);
+        } else {
+            return defaultValue;
+        }
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        return new HashMap<>(storage);
+    }
+}
+// END
