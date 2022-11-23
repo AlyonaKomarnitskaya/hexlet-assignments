bf4f796af795855ccf7d681845171193c5cc492e
@hexlet/cli <support@hexlet.io>
2022-11-23 06:21:14 +0500

    submit java-web-ru/html

---
+++ java-web-ru/html/src/main/java/exercise/servlet/UsersServlet.java
@@ -0,0 +1,152 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class UsersServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String pathInfo = request.getPathInfo();
+
+        if (pathInfo == null) {
+            showUsers(request, response);
+            return;
+        }
+
+        String[] pathParts = pathInfo.split("/");
+        String id = ArrayUtils.get(pathParts, 1, "");
+
+        showUser(request, response, id);
+    }
+
+    private List getUsers() throws JsonProcessingException, IOException {
+        // BEGIN
+        ObjectMapper mapper = new ObjectMapper();
+        Path path = Paths.get("src/main/resources/users.json").toAbsolutePath().normalize();
+        String pathN = Files.readString(path);
+        return mapper.readValue(pathN, List.class);
+
+        // END
+    }
+
+    private void showUsers(HttpServletRequest request,
+                           HttpServletResponse response)
+            throws IOException {
+
+        // BEGIN
+        List<Map> user = getUsers();
+        StringBuilder body = new StringBuilder();
+        body.append("""
+                <!DOCTYPE html>
+                <html lang=\"ru\">
+                    <head>
+                        <meta charset=\"UTF-8\">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+                                    rel="stylesheet"
+                                    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+                                    crossorigin="anonymous">
+                    </head>
+                    <body>
+                    
+                """);
+
+        for (Map<String, String> users : user) {
+            String id = users.get("id");
+            String fullName = users.get("firstName") + " " + users.get("lastName");
+
+            body.append("<tr>");
+            body.append("<td>" + id + "</id>");
+            body.append("<td><a href=\"/user/" + id + "\">" + fullName + "</a></id>");
+            body.append("</tr>");
+        }
+
+        body.append("""
+                          </table>
+                       </div>
+                    </body>
+                </html>
+                """);
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println(body.toString());
+        // END
+    }
+
+    private void showUser(HttpServletRequest request,
+                          HttpServletResponse response,
+                          String id)
+            throws IOException {
+
+        // BEGIN
+        List<Map> user = getUsers();
+        Map<String, String> users = user
+                .stream()
+                .filter(u -> u.get("id").equals(id))
+                .findAny()
+                .orElse(null);
+
+        if (users == null) {
+            response.sendError(404);
+            return;
+        }
+
+                StringBuilder body = new StringBuilder();
+                body.append("""
+                        <!DOCTYPE html>
+                        <html lang=\"ru\">
+                            <head>
+                                <meta charset=\"UTF-8\">
+                                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+                                            rel="stylesheet"
+                                            integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+                                            crossorigin="anonymous">
+                            </head>
+                            <body>
+                            
+                            
+                        """);
+
+                for (Map.Entry<String, String> entry : users.entrySet()) {
+                    body.append("<div>");
+                    body.append(entry.getKey() + ": " + entry.getValue());
+                    body.append("</div>");
+                }
+
+                body.append("""
+                                  </table>
+                               </div>
+                            </body>
+                        </html>
+                        """);
+
+                response.setContentType("text/html;charset=UTF-8");
+
+                PrintWriter out = response.getWriter();
+                out.println(body.toString());
+                // END
+            }
+        }
+
